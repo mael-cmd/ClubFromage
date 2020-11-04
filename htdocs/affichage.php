8ec4b7aa5e90e36b44a6eb0574b8b62f9596327c
Maël Chanliau <mael.chanliau@saintmichelannecy.fr>
2020-11-04 11:39:40 +0100

    04/11 11h38

--- htdocs/affichage.php
+++ htdocs/affichage.php
@@ -1,19 +1,20 @@
 <?php
+
+
 // require("model\business/fromage.php");/
 // require("model/business/membre.php");
 // require("model/business/pays.php");
 // require("model/business/avis.php");
-require('Autoloader.php');
-Autoloader::register();
+require_once 'vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use model\business\fromage;
 
-$t = array('identifiant'=>1,'Nom'=>'Camembert');
-$camembert = new \model\business\fromage($t);
+// create a log channel
+$log = new Logger('logs');
+$log->pushHandler(new StreamHandler(__DIR__.'\logs.log', Logger::WARNING));
 
-$t = array('identifiant'=>2,'Nom'=>'roquefort');
-$roquefort = new \model\business\fromage($t);
-
-echo $camembert -> _Nom;
-echo $camembert -> _identifiant;
-
-echo $roquefort -> _Nom;
-echo $roquefort -> _identifiant;
+// add records to the log
+$log->addWarning('Foo');
+$log->addError('Bar');
+echo "test";
